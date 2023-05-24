179d69fcdf264955402eb7dacff8279b68eeda14
José Luis Reyes <86855612+stinrcom@users.noreply.github.com>
2023-05-23 19:55:33 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Redait Media</title>
+</head>
+<body>
+<h1 align="center">Buenos días, estudiantes </h1>
+<br>
+<div align="center">
+<img src="playa2.jpg">
+</div>
+</body>
+</html>
